2672e4e5ea3a885070c9eac088a1b2c5a63f5f36
Jeremy Cowgar <jeremy@cowgar.com>
2019-12-07 09:58:22 -0500

    Initial import

---
+++ example/smart_arg_example.dart
@@ -0,0 +1,6 @@
+import 'package:smart_arg/smart_arg.dart';
+
+main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/smart_arg.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library smart_arg;
+
+export 'src/smart_arg_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
