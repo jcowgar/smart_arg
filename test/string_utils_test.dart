17d71b776412a6b33b35e5f13bb79f50bb028ba2
Jeremy Cowgar <jeremy@cowgar.com>
2019-12-11 11:37:09 -0700

    Final tests for string utils

--- test/string_utils_test.dart
+++ test/string_utils_test.dart
@@ -25,9 +25,9 @@ void main() {
 
     test('hardWrap', () {
       expect(hardWrap('Hello Hello', 7), 'Hello\nHello');
-      // FAILING: expect(hardWrap('Hello Hello', 5), 'Hello\nHello');
       expect(hardWrap('Hello World how are you doing?', 14),
           'Hello World\nhow are you\ndoing?');
+      expect(hardWrap('0123456789', 5), '01234\n56789');
     });
 
     test('indent', () {
