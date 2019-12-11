a53fbdfa49dcc8c1588f8cffd97d48ba88482f2c
Jeremy Cowgar <jeremy@cowgar.com>
2019-12-11 11:41:21 -0700

    Feature/9 wrap help (#14)

--- lib/src/smart_arg.dart
+++ lib/src/smart_arg.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 import 'dart:mirrors';
 
 import 'package:smart_arg/smart_arg.dart';
+import 'package:smart_arg/src/string_utils.dart';
 
 import 'argument.dart';
 import 'mirror_argument_pair.dart';
@@ -107,41 +108,30 @@ class SmartArg {
       helpDescriptions.add(helpLines);
     }
 
-    final maxKeyLenAllowed = 30;
+    const maxKeyLenAllowed = 20;
     final maxKeyLen = helpKeys.fold<int>(0,
         (a, b) => b.length > a && b.length < maxKeyLenAllowed ? b.length : a);
     final keyPadWidth = min(maxKeyLenAllowed, maxKeyLen + 1);
-    final continuedLineHelpTextPadding = ' '.padRight(keyPadWidth);
 
     for (var i = 0; i < helpKeys.length; i++) {
       var keyDisplay = helpKeys[i].padRight(keyPadWidth);
 
+      var thisHelpDescriptions = helpDescriptions[i].join('\n');
+      thisHelpDescriptions = hardWrap(thisHelpDescriptions, 78 - keyPadWidth);
+      thisHelpDescriptions = indent(thisHelpDescriptions, keyPadWidth);
+
       if (keyDisplay.length == keyPadWidth) {
-        final thisHelpDescriptions = helpDescriptions[i];
-        final firstHelpDisplay = thisHelpDescriptions.first;
-
-        lines.add('$keyDisplay$firstHelpDisplay');
-
-        for (var j = 1; j < thisHelpDescriptions.length; j++) {
-          lines.add('$continuedLineHelpTextPadding${thisHelpDescriptions[j]}');
-        }
+        thisHelpDescriptions =
+            thisHelpDescriptions.replaceRange(0, keyPadWidth, keyDisplay);
       } else {
-        final thisHelpDescriptions = helpDescriptions[i];
-
         lines.add(keyDisplay);
-        for (var j = 0; j < thisHelpDescriptions.length; j++) {
-          lines.add('$continuedLineHelpTextPadding${thisHelpDescriptions[j]}');
-        }
       }
+      lines.add(thisHelpDescriptions);
     }
 
     if (_app?.extendedHelp != null) {
       lines.add(' ');
-      lines.addAll(_app.extendedHelp
-          .trim()
-          .split(_eolRegex)
-          .map((v) => v.trim())
-          .toList());
+      lines.add(hardWrap(_app.extendedHelp, 78));
     }
 
     return lines.join('\n');
@@ -159,8 +149,6 @@ class SmartArg {
   // tracked so we can have a proper order for help output
   List<MirrorParameterPair> _mirrorParameterPairs;
 
-  final _eolRegex = RegExp(r'\r\n|[\r\n]');
-
   bool _isStacked(String value) {
     final isSingleDash = value.startsWith('-') && !value.startsWith('--');
     final isLongerThanShort = value.length > 2;
@@ -169,19 +157,24 @@ class SmartArg {
     return isSingleDash && !isAssignment && isLongerThanShort;
   }
 
-  bool _parse(List<String> arguments) {
-    final instanceMirror = reflect(this);
-    final List<String> expandedArguments = [];
-
+  List<String> _rewriteArguments(List<String> arguments) {
+    List<String> result = [];
     for (final arg in arguments) {
       if (_isStacked(arg)) {
         final individualArgs = arg.split('').skip(1).map((v) => '-$v').toList();
 
-        expandedArguments.addAll(individualArgs);
+        result.addAll(individualArgs);
       } else {
-        expandedArguments.add(arg);
+        result.add(arg);
       }
     }
+
+    return result;
+  }
+
+  bool _parse(List<String> arguments) {
+    final instanceMirror = reflect(this);
+    final List<String> expandedArguments = _rewriteArguments(arguments);
 
     int argumentIndex = 0;
     while (argumentIndex < expandedArguments.length) {
--- lib/src/string_utils.dart
+++ lib/src/string_utils.dart
@@ -47,22 +47,3 @@ String indent(String s, int indentCount, [String withChar = ' ']) {
   final eol = endOfLineOf(s) ?? '\n';
   return splitByEOL(s).map((v) => (withChar * indentCount) + v).join(eol);
 }
-
-void main() {
-  var s = r'''
-This pattern will split paragraphs into lines no longer than 35 characters each. It is designed to break on an EOL character, on one or more spaces, or at the end of the document. It will eliminate the extra spaces, if any, at the end of        the line. If a paragraph contains a really, really, really long, spaceless line, like this:
-
-1234567890123456789012345678901234567890123456789012345678901234567890
-
-It will break at exactly 35 characters. Note that the only side-effect is that you will get a trailing EOL at the end of the match.
-
-The ">" in the Replace Pattern is to demonstrate how to emulate e-mail style quoting. Remove it if you just want to wrap the lines, or replace it with "\t" or spaces if you want to indent each line.
-  ''';
-
-  var t = hardWrap(s, 40);
-  t = indent(t, 20);
-  print(t);
-
-  print(endOfLineOf('ss\r\naa').codeUnits);
-  print(replaceLineEndings('ss\ntt', 'xx'));
-}
