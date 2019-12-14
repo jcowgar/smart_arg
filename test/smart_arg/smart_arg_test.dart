7415d77ab3c904cd5f816d82eac1315b15e5f9ba
Jeremy Cowgar <jeremy@cowgar.com>
2019-12-14 18:13:16 -0500

    Merge commands test into big smart_arg test
    This gets around the issue of multiple reflectables for now

--- test/smart_arg/smart_arg_test.dart
+++ test/smart_arg/smart_arg_test.dart
@@ -266,6 +266,45 @@ class TestArgumentGroups extends SmartArg {
   int count = 1;
 }
 
+String whatExecuted;
+
+@SmartArg.parser
+@Parser(exitOnFailure: false, description: 'put command')
+class PutCommand extends SmartArgCommand {
+  @StringArgument()
+  String filename;
+
+  @override
+  void execute(SmartArg parentArguments) {
+    whatExecuted = 'put-command: $filename';
+  }
+}
+
+@SmartArg.parser
+@Parser(exitOnFailure: false, description: 'get command')
+class GetCommand extends SmartArgCommand {
+  @StringArgument()
+  String filename;
+
+  @override
+  void execute(SmartArg parentArguments) {
+    whatExecuted = 'get-command: $filename';
+  }
+}
+
+@SmartArg.parser
+@Parser(exitOnFailure: false)
+class TestSimpleCommand extends SmartArg {
+  @BooleanArgument(short: 'v')
+  bool verbose;
+
+  @Command(help: 'Put a file on a remote host')
+  PutCommand put;
+
+  @Command(help: 'Get a file from a remote host')
+  GetCommand get;
+}
+
 void main() {
   initializeReflectable();
 
@@ -668,6 +707,42 @@ void main() {
         final args = TestFileDirectoryMustExist()..parse(['--directory=./lib']);
         expect(args.directory.path, contains('lib'));
       });
+    });
+  });
+
+  group('commands', () {
+    setUp(() {
+      whatExecuted = null;
+    });
+
+    test('executes with no arguments', () {
+      final args = TestSimpleCommand()..parse([]);
+      expect(args.verbose, null);
+    });
+
+    test('executes with a command', () {
+      final args = TestSimpleCommand()
+        ..parse(['-v', 'put', '--filename=upload.txt']);
+      expect(args.verbose, true);
+      expect(whatExecuted, 'put-command: upload.txt');
+    });
+
+    test('executes with another command', () {
+      final args = TestSimpleCommand()
+        ..parse(['-v', 'get', '--filename=download.txt']);
+      expect(args.verbose, true);
+      expect(whatExecuted, 'get-command: download.txt');
+    });
+
+    test('help appears', () {
+      final args = TestSimpleCommand();
+      final help = args.usage();
+
+      expect(help.contains('COMMANDS'), true);
+      expect(help.contains('  get'), true);
+      expect(help.contains('  put'), true);
+      expect(help.contains('Get a file'), true);
+      expect(help.contains('Put a file'), true);
     });
   });
 
