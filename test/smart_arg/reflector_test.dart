c45318efb704182a6996d3b1287c42db3dd2b6ba
Jeremy Cowgar <jeremy@cowgar.com>
2019-12-14 23:14:27 -0500

    Fix unused variable error

--- test/smart_arg/reflector_test.dart
+++ test/smart_arg/reflector_test.dart
@@ -5,7 +5,7 @@ import 'package:smart_arg/smart_arg.dart';
 void main() {
   group('reflector', () {
     test('constructs', () {
-      final reflector = Reflector();
+      final _ = Reflector();
       expect(1, 1);
     });
   });
